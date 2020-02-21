c75a4c1d5e90c7a91f4f2cb9e5c7d5faf151c0c8
chibi19 <58395801+chibi19@users.noreply.github.com>
2020-02-21 18:16:24 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -13,6 +13,8 @@ const DATA  = {
 	deadlinePercent:[20, 17, 15]
 };
 
+const DAY_STRING = ['день', 'дня', 'дней'];
+
 const startButton = document.querySelector('.start-button'),
 	firstScreen = document.querySelector('.first-screen'),
 	mainForm = document.querySelector('.main-form'),
@@ -20,7 +22,19 @@ const startButton = document.querySelector('.start-button'),
 	endButton = document.querySelector('.end-button'),
 	total = document.querySelector('.total'),
 	fastRange = document.querySelector('.fast-range'),
-	totalPriceSum = document.querySelector('.total_price__sum');
+	totalPriceSum = document.querySelector('.total_price__sum'),
+	adapt = document.getElementById('adapt'),
+	mobileTemplates = document.getElementById('mobileTemplates'),
+	typeSite = document.querySelector('.type-site'),
+	maxDeadline = document.querySelector('.max-deadline'),
+	rangeDeadline = document.querySelector('.range-deadline'),
+	deadlineValue = document.querySelector('.deadline-value');
+
+
+function declOfNum(n, titles) {
+		return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+			0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+	}
 
 function showElem(elem) {
 	elem.style.display = 'block';
@@ -29,13 +43,29 @@ function showElem(elem) {
 
 function hideElem(elem) {
 	elem.style.display = 'none';
-	
+
 }
+
+
+function renderTextContent(total, site, maxDay, minDay) {
+		totalPriceSum.textContent = total;
+		typeSite.textContent = site;
+		maxDeadlineDay.textContent = declOfNum(maxDay,DAY_STRING);
+		rangeDeadline.min = minDay;
+		rangeDeadline.max = maxDay;
+		deadlineValue.textContent = declOfNum(rangeDeadline.value, DAY_STRING)
+
+}
+	
+
 
 function priceCalculation(elem) {
      let result = 0;
          index = 0,
-         options = [];
+         options = [],
+         site = '',
+         maxDeadlineDay = DATA.deadlineDay[index][1];
+         minDeadlineDay = DATA.deadlineDay[index][0];
 
 
 	if(elem.name === 'whichSite'){
@@ -49,7 +79,10 @@ function priceCalculation(elem) {
 
 	for(const item of formCalculate.elements){
 		if (item.name === 'whichSite' && item.checked){
-			index = (DATA.whichSite.indexOf(item.value))
+			index = DATA.whichSite.indexOf(item.value);
+			site = item.dataset.site;
+			maxDeadlineDay = DATA.deadlineDay[index][1];
+			minDeadlineDay = DATA.deadlineDay[index][0];
 		} else if (item.classList.contains('calc-handler')&& item.checked){
 			 options.push(item.value)
 		}
@@ -73,12 +106,21 @@ function priceCalculation(elem) {
 
 	result += DATA.price[index];
 
-	totalPriceSum.textContent = result;
+	renderTextContent(result, site, maxDeadlineDay, minDeadlineDay);
+
+	
 }
 
 
 function handlerCallBackForm(event) {
 	const target = event.target;
+
+	if (adapt.checked) {
+		mobileTemplates.disabled = false;
+	}else {
+		mobileTemplates.disabled = true;
+		mobileTemplates.checked = false;
+	}
 
 	if(target.classList.contains('want-faster')){
 		target.checked ? showElem(fastRange) : hideElem(fastRange);
